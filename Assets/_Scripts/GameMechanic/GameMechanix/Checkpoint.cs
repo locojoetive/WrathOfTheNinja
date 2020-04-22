8c36861dd229403e89d6e066ead5de0fb6d1039b
locojoetive <johannes.mic@gmail.com>
2020-04-22 23:54:25 +0200

    [locojoetive] adds mobile and joypad feature

---
+++ Assets/_Scripts/GameMechanic/GameMechanix/Checkpoint.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    public int newCheckpoint = 0;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        StageManager.changeCurrentCheckpoint(newCheckpoint);
+    }
+}
